05ed67b36fa8275b4e11c453ade0aad207ae67e7
Ivan Yan <yan.2314@gmail.com>
2017-05-17 11:43:58 +0800

    add source

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# repo: https://github.com/w3c/csswg-drafts/
+
+curl -o index.html https://drafts.csswg.org/css-animations/
+curl -O https://drafts.csswg.org/css-animations/animation1.png
